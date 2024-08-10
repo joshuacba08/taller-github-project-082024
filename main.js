39bd1744c37894e87821ca36076bc0fc3b6d322c
Josue Oroya <joshuacba08@gmail.com>
2024-08-10 10:18:12 -0300

    change name

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import javascriptLogo from './javascript.svg'
+import './style.css'
+import viteLogo from '/vite.svg'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -11,7 +11,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hello Josue!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
